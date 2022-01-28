ad41bf4b326a0c693684383548c1767529dec480
DevExpressExampleBot <fakeEmail>
2022-01-28 15:07:35 +0300

    Source auto update [skip ci]

--- VB/MethodToCommandExample/App.xaml.vb
+++ VB/MethodToCommandExample/App.xaml.vb
@@ -1,14 +1,11 @@
-﻿Imports System
-Imports System.Linq
 Imports System.Windows
-Imports System.Collections.Generic
 
 Namespace MethodToCommandExample
-    Partial Public Class App
+
+    Public Partial Class App
         Inherits Application
 
         Private Sub OnAppStartup_UpdateThemeName(ByVal sender As Object, ByVal e As StartupEventArgs)
-
             DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName()
         End Sub
     End Class
--- VB/MethodToCommandExample/ViewModel/MainViewModel.vb
+++ VB/MethodToCommandExample/ViewModel/MainViewModel.vb
@@ -1,12 +1,11 @@
-﻿Imports System
-Imports System.Linq
 Imports DevExpress.Mvvm.POCO
-Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 Imports MethodToCommandExample.Common
 
 Namespace MethodToCommandExample.ViewModel
+
     Public Class MainViewModel
+
         Public Sub New()
             Users = New ObservableCollection(Of User)() From {User.Create(0, "Jack"), User.Create(1, "Ron"), User.Create(2, "John"), User.Create(3, "Antoni"), User.Create(4, "Paul")}
         End Sub
@@ -15,6 +14,6 @@ Namespace MethodToCommandExample.ViewModel
             Return ViewModelSource.Create(Function() New MainViewModel())
         End Function
 
-        Public Overridable Property Users() As ObservableCollection(Of User)
+        Public Overridable Property Users As ObservableCollection(Of User)
     End Class
 End Namespace
